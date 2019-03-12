a28428411b6ad42c09c90a5065feab735abbf931
Anthony Jaspers <jaspers.anthony@gmail.com>
2019-03-12 10:57:55 +0100

    premier commit bois 2 (working!)

--- CodeALaMode.py
+++ CodeALaMode.py
@@ -1,6 +1,14 @@
 import sys
 import math
 
+CHOPPED_STRAWBERRIES = "CHOPPED_STRAWBERRIES"
+STRAWBERRIES = "STRAWBERRIES"
+NONE = "NONE"
+BLUEBERRIES = "BLUEBERRIES"
+DISH = "DISH"
+ICE_CREAM = "ICE_CREAM"
+CHOPPING_BOARD = "CHOPPING_BOARD"
+
 
 def print_err(string):
     print(string, file=sys.stderr)
@@ -13,6 +21,10 @@ class Chef:
         self.x = int(x)
         self.y = int(y)
         self.item = item.split("-")
+        self.is_chopping = False
+        self.chopped_item = "EMPTY"
+        self.last_dish_coords = "EMPTY"
+        self.chopping_state = "EMPTY"
 
     def update_info(self, x, y, item):
         self.x = int(x)
@@ -22,26 +34,84 @@ class Chef:
     def show_items(self):
         print_err(self.name + " : " + " & ".join(self.item))
 
+    def is_around_coords(self, x, y):
+        x = int(x)
+        y = int(y)
+        if x >= self.x-1 and x <= self.x+1:
+            if y >= self.y-1 and y <= self.y+1:
+                return True
+        return False
+
     def chop_item(self, item):
-        if item == "CHOPPED_STRAWBERRIES":
-            if "STRAWBERRIES" not in self.item:
-                return "USE " + Kitchen.get_coords("S")
-            else:
+        if item == CHOPPED_STRAWBERRIES:
+            '''
+            1) poser son assiettes pres de la planche
+            2) prendre des fraises
+            3) couper les fraises
+            4) poser sur l'assiette
+            5) prendre l'assiette
+            '''
 
-                if self.is_around("C") == False:
-                   is_near_board = False
-                else:
-                    is_near_board = True
+            return_action = "ERROR IN CHOP_ITEM"
+            print_err("Choppin State = " + self.chopping_state)
 
-                if "NONE" not in self.item:
-                    if not is_near_board:
-                        return "MOVE " + Kitchen.get_coords("C")
+            if self.chopping_state == "SEARCH_CHOPPED":
+                found_chopped = False
+                for it in TableItem.list:
+                    if it.item == CHOPPED_STRAWBERRIES:
+                        found_chopped = True
+                        return_action = "USE %d %d" % (it.x, it.y)
+                        if self.is_around_coords(it.x, it.y):
+                            self.is_chopping = False
+                            self.chopping_state = "EMPTY"
+                if self.is_chopping and not found_chopped:
+                    if self.is_around("C") != False:
+                        self.chopping_state = "SEARCH_BOARD"
                     else:
-                        return "USE " + self.is_around("#")
+                        self.chopping_state = "DROP_DISH"
+
+            if self.chopping_state == "SEARCH_BOARD":
+                return_action = "MOVE " + Kitchen.get_coords("C")
+
+                if self.is_around("C") != False:
+                    self.chopping_state = "DROP_DISH"
+
+            if self.chopping_state == "DROP_DISH":
+                    free_table_coords = self.is_around("#")
+                    self.last_dish_coords = free_table_coords
+                    return_action = "USE " + free_table_coords
+                    if NONE in self.item:
+                        self.chopping_state = "SEARCH_ITEM"
+
+            if self.chopping_state == "SEARCH_ITEM":
+                if STRAWBERRIES not in self.item:
+                    return_action =  "USE " + Kitchen.get_coords("S")
+                else:
+                    self.chopping_state = "CHOP_ITEM"
+
+            if self.chopping_state == "CHOP_ITEM":
+                if CHOPPED_STRAWBERRIES not in self.item:
+                    return_action = "USE " + Kitchen.get_coords("C")
+                else:
+                    self.chopping_state = "USE_DISH"
+
+            if self.chopping_state == "USE_DISH":
+                last_coords = self.last_dish_coords
+                #x, y = last_coords.split(" ")
+
+                if DISH not in self.item:
+                    return_action = "USE " + last_coords
+                else:
+                    last_coords = "EMPTY"
+                    self.is_chopping = False
+                    return_action = "WAIT"
+
+            print_err("Choppin State = " + self.chopping_state)
+            return return_action
 
     def is_around(self, target):
-        for iy in range(-1, 1):
-            for ix in range(-1, 1):
+        for iy in range(-1, 2):
+            for ix in range(-1, 2):
                 look_x = self.x + ix
                 look_y = self.y + iy
                 if Kitchen.map[look_y][look_x] == target:
@@ -77,18 +147,19 @@ class Kitchen:
 
     @staticmethod
     def get_initial(item):
-        if item == "DISH":
+        if item == DISH:
             return "D"
-        if item == "ICE_CREAM":
+        if item == ICE_CREAM:
             return "I"
-        if item == "BLUEBERRIES":
+        if item == BLUEBERRIES:
             return "B"
-        if item == "STRAWBERRIES":
+        if item == STRAWBERRIES:
             return "S"
-        if item == "CHOPPING_BOARD":
+        if item == CHOPPING_BOARD:
             return "C"
-        if item == "CHOPPED_STRAWBERRIES":
+        if item == CHOPPED_STRAWBERRIES:
             return None
+
 
 class Customer:
     nb_customers = 0
@@ -112,6 +183,16 @@ class Customer:
         for cus in Customer.waiting_list:
             print_err("(%d) item:%s / award:%d" % (cus.id, "-".join(cus.item), cus.award))
             # (1) item:DISH-BLUEBERRIES-ICE_CREAM / award:650
+
+    @staticmethod
+    def get_best_order_item():
+        best_item = ''
+        award=0
+        for cus in Customer.waiting_list:
+            if cus.award > award:
+                award = cus.award
+                best_item = cus.item
+        return best_item
 
 
 class TableItem:
@@ -140,6 +221,7 @@ class TableItem:
 
 show_waiting = False
 show_table_items = False
+show_active_order = True
 
 
 num_all_customers = int(input())
@@ -158,9 +240,12 @@ Kitchen.show_map()
 
 myChef = Chef("My Chef", 99, 99, "")
 partnerChef = Chef("My Partner", 99, 99, "")
+can_steal = False
 
 # game loop
 while True:
+    print_err("== Is chopping ? %s" % str(myChef.is_chopping))
+
     turns_remaining = int(input())
 
     player_x, player_y, player_item = input().split()
@@ -201,36 +286,44 @@ while True:
 
     myChef.show_items()
 
-    can_steal = False
-    for ti in TableItem.list:
-        # print_err(ti.item_is_ordered(Customer.waiting_list))
-        get_stealable_coord = ti.item_is_ordered(Customer.waiting_list)
-        if get_stealable_coord != None:
-            next_action = "USE " + get_stealable_coord
-            can_steal = True
+    if not myChef.is_chopping:
+        if NONE in myChef.item:
+            for ti in TableItem.list:
+                if DISH in ti.item:
+                    can_steal = True
+                    next_action = "USE %d %d" % (ti.x, ti.y)
+            if next_action == "WAIT":
+                can_steal = False
+
+        # if can_steal and NONE not in myChef.item:
+        #    next_action = "USE " + Kitchen.get_coords("D")
+
 
     if not can_steal or "NONE" not in myChef.item:
 
-        if "NONE" in myChef.item:
+        if myChef.is_chopping:
+            next_action = myChef.chop_item(myChef.chopped_item)
+        elif "NONE" in myChef.item:
             next_action = "USE " + Kitchen.get_coords("D")
         else:
+            if show_active_order:
+                print_err("My Active Order: %s" % ("-".join(Customer.waiting_list[2].item)))
             for cus_item in Customer.waiting_list[2].item:
-                print_err("2:"+cus_item)
                 if cus_item not in myChef.item:
-                    if "CHOPPED" in cus_item.split("_"):
+                    if "CHOPPED" in cus_item.split("_") and not myChef.is_chopping:
+                        myChef.is_chopping = True
+                        myChef.chopping_state = "SEARCH_CHOPPED"
+                        myChef.chopped_item = cus_item
                         next_action = myChef.chop_item(cus_item)
-                        print_err("3_chop:" + next_action)
+                        break
                     else:
-                        print_err("3:" + Kitchen.get_coords(Kitchen.get_initial(cus_item[0])))
-                        next_action = "USE " + Kitchen.get_coords(Kitchen.get_initial(cus_item[0]))
-
+                        next_action = "USE " + Kitchen.get_coords(Kitchen.get_initial(cus_item))
+                        break
+            if next_action == "WAIT":
+                next_action = "USE " + Kitchen.get_coords("W")
     else:
-        if "NONE" in myChef.item:
-            pass
-        else:
+        if "NONE" not in myChef.item:
             next_action = "USE " + Kitchen.get_coords("W")
-
-
 
     # ========= ACTION ===========================
     print(next_action)
